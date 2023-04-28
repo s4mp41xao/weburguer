67cff5e22677a95100da8e2b0c6507b1e50d1928
s4mp41xao <samribe@outloook.com>
2023-04-28 18:04:28 -0300

    Hero component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
-import Navbar from "./components/Navbar";
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 function App() {
   return (
     <div>
-        <Navbar />
+      <Navbar />
+      <Hero />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
