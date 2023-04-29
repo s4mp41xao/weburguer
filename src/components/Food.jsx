4c941806233bb8860865ca2d961719b9f6a06f70
s4mp41xao <samribe@outloook.com>
2023-04-28 21:14:50 -0300

    Map through data

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {data} from "../data/data.js";
 
 const Food = () => {
-  console.log(data);
+  // console.log(data);
+
+const [foods, setFoods] = useState(data)
+
   return (
     <div className='max-w-[1640px] m-auto px-4 py-12'>
 
@@ -38,8 +41,20 @@ const Food = () => {
 
       </div>
 
-      <div>
-        
+      {/* Display foods */}
+      <div className='grid gird-cols-2 lg:grid-cols-4 gap-6 pt-4'>
+        {foods.map((item, index)=> (
+          <div key={index} className='border shadow-lg rounded-lg hover:scale-105 duration-300'>
+            <img src={item.image} alt={item.name}
+            className='w-full h-[200px] object-cover rounded-t-lg' />
+            <div className='flex justify-between px-2 py-4'>
+              <p className='font-bold'>{item.name}</p>
+              <p>
+                <span className='bg-orange-500 text-white p-1 rounded-full'>{item.price}</span>
+              </p>
+            </div>
+          </div>
+        ) )}
       </div>
 
     </div>
