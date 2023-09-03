a214f435a4d47830cab83b8fcb7b5cb2cbbdc361
s4mp41xao <samribe@outlook.com>
2023-09-02 21:22:47 -0300

    preview-changes-test

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,6 +11,8 @@ const Hero = () => {
           font-bold'>O sabor que<span className='text-orange-500'> você merece</span></h1>
           <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl
           font-bold'><span className='text-orange-500'>em cada</span>  mordida.</h1>
+          <p>Teste</p>
+          <h2>Hello</h2>
         </div>
         <img className='w-full max-h-[500px] object-cover' src="https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="/" />
       </div>
